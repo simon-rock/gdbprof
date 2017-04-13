28b962c858db348b821cb5cbd35900ca4439c607
Mark Nelson <mnelson@redhat.com>
2017-04-12 23:20:43 -0400

    Very hacky first attempt at regular and inverse callgraphs.
    Signed-off-by: Mark Nelson <mnelson@redhat.com>

--- gdbprof.py
+++ gdbprof.py
@@ -25,7 +25,6 @@ from time import sleep
 import os
 import signal
 
-
 def get_call_chain():
     function_names = []
     frame = gdb.newest_frame()
@@ -35,6 +34,81 @@ def get_call_chain():
 
     return tuple(function_names)
 
+class Function:
+
+    def __init__(self, name, indent):
+        self.name = name
+        self.indent = indent
+        self.subfunctions = []
+
+        # count of times we terminated here
+        self.count = 0
+
+    def add_count(self):
+        self.count += 1
+
+    def get_samples(self):
+        _count = self.count
+        for function in self.subfunctions:
+            _count += function.get_samples()
+        return _count
+
+    def get_percent(self, total):
+        return 100.0 * self.get_samples() / total
+
+    def get_name(self):
+        return self.name;
+
+    def get_func(self, name):
+        for function in self.subfunctions:
+          if function.get_name() == name:
+            return function
+        return None
+
+    def get_or_add_func(self, name):
+        function = self.get_func(name);
+        if function is not None:
+            return function; 
+        function = Function(name, self.indent)
+        self.subfunctions.append(function)
+        return function
+
+    def print_samples(self, depth):
+        print "%s%s - %s" % (' ' * (self.indent * depth), self.get_samples(), self.name)
+        for function in self.subfunctions:
+            function.print_samples(depth+1)
+
+    def print_percent(self, prefix, total):
+#        print "%s%0.2f - %s" % (' ' * (self.indent * depth), self.get_percent(total), self.name)
+        subfunctions = {}
+        for function in self.subfunctions:
+            subfunctions[function.name] = function.get_percent(total)
+        
+        i = 0
+        for name, value in sorted(subfunctions.iteritems(), key=lambda (k,v): (v,k), reverse=True):
+            new_prefix = '' 
+            if i + 1 == len(self.subfunctions):
+               new_prefix += '  '
+            else:
+               new_prefix += '| '
+
+            print "%s%s%0.2f%% %s" % (prefix, "+ ", value, name)
+            self.get_func(name).print_percent(prefix + new_prefix, total)
+            i += 1
+
+    def add_frame(self, frame):
+        if frame is None:
+            self.count += 1
+        else:
+            function = self.get_or_add_func(frame.name())
+            function.add_frame(frame.older())
+
+    def inverse_add_frame(self, frame):
+        if frame is None:
+            self.count += 1
+        else:
+            function = self.get_or_add_func(frame.name())
+            function.inverse_add_frame(frame.newer())
 
 class ProfileCommand(gdb.Command):
     """Wall clock time profiling leveraging gdb for better backtraces."""
@@ -42,7 +116,6 @@ class ProfileCommand(gdb.Command):
     def __init__(self):
         super(ProfileCommand, self).__init__("profile", gdb.COMMAND_RUNNING,
                                              gdb.COMPLETE_NONE, True)
-
 
 class ProfileBeginCommand(gdb.Command):
     """Profile an application against wall clock time.
@@ -74,10 +147,11 @@ The default PERIOD is 0.5 seconds.
             sleep(period)
             os.kill(gdb.selected_inferior().pid, signal.SIGINT)
 
-        call_chain_frequencies = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
+#        call_chain_frequencies = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
+        top = Function("Top", 2)
         sleeps = 0
 
-        for i in xrange(0,5000):
+        for i in xrange(0,100):
           gdb.events.cont.connect(breaking_continue_handler)
           gdb.execute("continue", to_string=True)
           gdb.events.cont.disconnect(breaking_continue_handler)
@@ -87,25 +161,32 @@ The default PERIOD is 0.5 seconds.
             for th in inf.threads():
               thn = th.num
               th.switch()
-              call_chain_frequencies[inum][thn][get_call_chain()] += 1
-
+#              call_chain_frequencies[inum][thn][get_call_chain()] += 1
+              frame = gdb.newest_frame()
+              while (frame.older() != None):
+                frame = frame.older()
+              top.inverse_add_frame(frame);
+#              top.add_frame(gdb.newest_frame())
           sleeps += 1
           gdb.write(".")
           gdb.flush(gdb.STDOUT)
 
-        print("\nProfiling complete with %d samples." % sleeps)
-        for inum, i_chain_frequencies in sorted(call_chain_frequencies.iteritems()):
-            print ""
-            print "INFERIOR NUM: %s" % inum
-            print ""
-            for thn, t_chain_frequencies in sorted (i_chain_frequencies.iteritems()):
-                print ""
-                print "THREAD NUM: %s" % thn
-                print ""
+        print "";
+        top.print_percent("", top.get_samples())
 
-                for call_chain, frequency in sorted(t_chain_frequencies.iteritems(), key=lambda x: x[1], reverse=True):
-                    print("%d\t%s" % (frequency, '->'.join(str(i) for i in call_chain)))
-
+#        print("\nProfiling complete with %d samples." % sleeps)
+#        for inum, i_chain_frequencies in sorted(call_chain_frequencies.iteritems()):
+#            print ""
+#            print "INFERIOR NUM: %s" % inum
+#            print ""
+#            for thn, t_chain_frequencies in sorted (i_chain_frequencies.iteritems()):
+#                print ""
+#                print "THREAD NUM: %s" % thn
+#                print ""
+#
+#                for call_chain, frequency in sorted(t_chain_frequencies.iteritems(), key=lambda x: x[1], reverse=True):
+#                    print("%d\t%s" % (frequency, '->'.join(str(i) for i in call_chain)))
+#
 #        for call_chain, frequency in sorted(call_chain_frequencies.iteritems(), key=lambda x: x[1], reverse=True):
 #            print("%d\t%s" % (frequency, '->'.join(str(i) for i in call_chain)))
 
